7caf17fc97a4cc25347209548ce2659f6de0025c
shaquibkhan <connectshaquib@gmail.com>
2017-04-26 20:44:11 +0530

    commit
    commit

---
+++ app1.js
@@ -0,0 +1,11 @@
+	// object is created named as student ...
+var student= {
+	name :"SHAQUIB AHMAD KHAN",
+    age:23,												// properties of obj..
+  	dob:"29 Dec 1993", 
+  	pob:"NEW-DELHI"
+			};
+			//obj is calling its properties
+console.log("Name:" +student.name+ " Age:" +student.age+ " Date of birth:" +student.dob+ " Place of birth:" +student.pob);
+
+
